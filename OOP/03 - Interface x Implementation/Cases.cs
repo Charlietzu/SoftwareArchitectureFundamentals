9959e7b26ca2ab81ef924cf13588903fff09b15a
Charlietzu <caiocesarsilva08@gmail.com>
2021-09-23 22:28:15 -0300

    Interface x Implementation

---
+++ OOP/03 - Interface x Implementation/Cases.cs
@@ -0,0 +1,67 @@
+﻿namespace OOP
+{
+    public interface IRepository
+    {
+        void Add();
+    }
+
+    public class Repository : IRepository
+    {
+        //public Repository(int a)
+        //{
+
+        //}
+
+        public void Add()
+        {
+            // Add item
+        }
+    }
+
+    public class RepositoryFake : IRepository
+    {
+        public void Add()
+        {
+            // Addd item
+        }
+    }
+
+    public class ImplementationUse
+    {
+        public void Process()
+        {
+            Repository repository = new Repository();
+            repository.Add();
+        }
+    }
+
+    public class AbstractionUse
+    {
+        private readonly IRepository _repository;
+
+        public AbstractionUse(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Process()
+        {
+            _repository.Add();
+        }
+    }
+
+    public class InterfaceImplementationTest
+    {
+        public InterfaceImplementationTest()
+        {
+            ImplementationUse repoImp = new ImplementationUse();
+            repoImp.Process();
+
+            AbstractionUse repoAbs = new AbstractionUse(new Repository());
+            repoAbs.Process();
+
+            AbstractionUse repoAbsFake = new AbstractionUse(new RepositoryFake());
+            repoAbsFake.Process();
+        }
+    }
+}
