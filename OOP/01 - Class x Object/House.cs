f3402bb2cb2c5349d229b7577f1d191c71eacff2
Charlietzu <caiocesarsilva08@gmail.com>
2021-09-23 22:09:31 -0300

    OOP - Class x Objects

---
+++ OOP/01 - Class x Object/House.cs
@@ -0,0 +1,26 @@
+﻿namespace OOP
+{
+    //Class definition
+    public class House
+    {
+        public int SizeSquareMeters { get; set; }
+        public int Floors { get; set; }
+        public decimal Value { get; set; }
+        public int VacanciesNumber { get; set; }
+    }
+
+    //Object definition
+    public class Object
+    {
+        public Object()
+        {
+            House house = new House
+            {
+                SizeSquareMeters = 100,
+                Floors = 2,
+                Value = 100000,
+                VacanciesNumber = 3
+            };
+        }
+    }
+}
