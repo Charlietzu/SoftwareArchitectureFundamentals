4e3b268e6cce10ba1c2b00f2f4ee2b5baae5199c
Charlietzu <caiocesarsilva08@gmail.com>
2021-09-24 21:19:46 -0300

    feat: LSP Solution and Violation examples

---
+++ SOLID/03 - LSP/LSP.Solution/CalculateArea.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SOLID.LSP.Solution
+{
+    public class CalculateArea
+    {
+        private static void GetParallelogramArea(Parallelogram parallelogram)
+        {
+            Console.Clear();
+            Console.WriteLine("Calculate Parallelogram Area");
+            Console.WriteLine();
+            Console.WriteLine(parallelogram.Height + " * " + parallelogram.Width);
+            Console.WriteLine();
+            Console.WriteLine(parallelogram.Area);
+            Console.ReadKey();
+        }
+
+        public static void Calcular()
+        {
+            Square quad = new Square(5, 5);
+            Rectangle ret = new Rectangle(10, 5);
+
+            GetParallelogramArea(quad);
+            GetParallelogramArea(ret);
+        }
+    }
+}
---
+++ SOLID/03 - LSP/LSP.Violation/CalculateArea.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SOLID.LSP.Violation
+{
+    public class CalculateArea
+    {
+        private static void GetRectangleArea(Rectangle rec)
+        {
+            Console.Clear();
+            Console.WriteLine("Calculate Rectangle Area");
+            Console.WriteLine();
+            Console.WriteLine(rec.Height + " * " + rec.Width);
+            Console.WriteLine();
+            Console.WriteLine(rec.Area);
+            Console.ReadKey();
+        }
+
+        public static void Calculate()
+        {
+            Square square = new Square()
+            {
+                Height = 10,
+                Width = 5
+            };
+
+            GetRectangleArea(square);
+        }
+    }
+}
