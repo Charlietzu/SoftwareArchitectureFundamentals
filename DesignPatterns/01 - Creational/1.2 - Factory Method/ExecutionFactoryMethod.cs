44f7bba0a79a9d42dc6a3fadd029d254af75a8f8
Charlietzu <caiocesarsilva08@gmail.com>
2021-09-27 23:00:45 -0300

    feat: Factory Method implementation example

---
+++ DesignPatterns/01 - Creational/1.2 - Factory Method/ExecutionFactoryMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DesignPatterns.FactoryMethod
+{
+    public class ExecutionFactoryMethod
+    {
+        public static void Execute()
+        {
+            Connection sqlCn = DbFactory.Database(DataBase.SqlServer)
+                                 .CreateConnector("myConnectionString")
+                                 .Connect();
+
+            sqlCn.ExecuteCommand("select * from tableSqlServer");
+            sqlCn.Close();
+
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("");
+
+            Connection oracleCn = DbFactory.Database(DataBase.Oracle)
+                                    .CreateConnector("myConnectionString")
+                                    .Connect();
+
+            oracleCn.ExecuteCommand("select * from tableOracle");
+            oracleCn.Close();
+        }
+    }
+}
