9f506ca3575d2604d6b01c670f2675295e1a9dec
Charlietzu <caiocesarsilva08@gmail.com>
2021-09-28 21:27:56 -0300

    feat: Singleton pattern implementation example

---
+++ DesignPatterns/01 - Creational/1.3 - Singleton/Server.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    internal class Server
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string IP { get; set; }
+    }
+}
---
+++ DesignPatterns/01 - Creational/1.3 - Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    public class ExecutionSingleton
+    {
+        public static void Execute()
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same Instance\n");
+            }
+
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Sending request to: " + balancer.NextServer.Name);
+            }
+        }
+    }
+}
