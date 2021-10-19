76b17f27ee588913da08a14baa72d99798f9d1c2
Caio César Silva <caiocesarsilva08@gmail.com>
2021-10-18 21:00:36 -0300

    refactor: assigning types to variables

--- DesignPatterns/02 - Structural/2.3 - Composite/CompositeExecution.cs
+++ DesignPatterns/02 - Structural/2.3 - Composite/CompositeExecution.cs
@@ -4,33 +4,33 @@
     {
         public static void Execute()
         {
-            var registrationValidation = new Message("The registration was not successful!");
+            Message registrationValidation = new Message("The registration was not successful!");
 
-            var userFormError = new Message("User entered an invalid name");
+            Message userFormError = new Message("User entered an invalid name");
 
-            var nameLengthForm = new InputFormMessage("Name must be more than 2 characters");
-            var emptyNameForm = new InputFormMessage("The name cannot have numbers");
+            InputFormMessage nameLengthForm = new InputFormMessage("Name must be more than 2 characters");
+            InputFormMessage emptyNameForm = new InputFormMessage("The name cannot have numbers");
 
             userFormError.AddChild(nameLengthForm);
             userFormError.AddChild(emptyNameForm);
 
             registrationValidation.AddChild(userFormError);
 
-            var userDomainError = new Message("Problems processing user registration");
+            Message userDomainError = new Message("Problems processing user registration");
 
-            var userCpfDomain = new DomainMessage("The CPF informed is in use!");
-            var userEmailDomain = new DomainMessage("The Email entered is in use!");
+            DomainMessage userCpfDomain = new DomainMessage("The CPF informed is in use!");
+            DomainMessage userEmailDomain = new DomainMessage("The Email entered is in use!");
 
             userDomainError.AddChild(userCpfDomain);
             userDomainError.AddChild(userEmailDomain);
 
             registrationValidation.AddChild(userDomainError);
 
-            var msgLevel1 = new Message("Level 1");
-            var msgLevel2 = new Message("Level 2");
-            var msgLevel3 = new Message("Level 3");
-            var msgLevel4 = new Message("Level 4");
-            var msgLevel5 = new Message("Level 5");
+            Message msgLevel1 = new Message("Level 1");
+            Message msgLevel2 = new Message("Level 2");
+            Message msgLevel3 = new Message("Level 3");
+            Message msgLevel4 = new Message("Level 4");
+            Message msgLevel5 = new Message("Level 5");
 
             msgLevel4.AddChild(msgLevel5);
             msgLevel3.AddChild(msgLevel4);
